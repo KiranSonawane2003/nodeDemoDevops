5c33d716f4b88bb887079dced471a4c2de2d4584
deepakd <deepakd@winjit.com>
2019-06-03 11:15:44 +0530

    this is new change

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'node devops testing' });
- // res.sendStatus(200)
+  //res.render('index', { title: 'node devops testing' });
+  res.send("This is updated push")
 });
 
 module.exports = router;
